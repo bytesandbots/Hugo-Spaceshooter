022d1f720d2d0004245c3502fe253a406de139e6
bytesandbots <students@bytesandbots.com>
2023-07-20 14:47:04 -0700

    I programed

---
+++ Assets/shooting.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public GameObject projectile;
+    public float frequency = .5f;
+    public Transform[] barrels;
+    private float ctime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ctime >= frequency)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                foreach (Transform barrel in barrels)
+                {
+                    GameObject clone = Instantiate(projectile, barrel.position, barrel.rotation);
+                    Destroy(clone, 3);
+                    ctime = 0;
+                }
+            }
+        }
+        else
+        {
+            ctime += Time.deltaTime;
+        }
+    }
+}
