62d342b2db3594af1a68ebf1e9d1cc8fa73b8902
bytesandbots <students@bytesandbots.com>
2023-07-23 16:00:59 -0700

    I added the boooooosssss

--- Assets/sooting.cs
+++ Assets/sooting.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class sooting : MonoBehaviour
 {
+    public GameObject projectil;
+    public Transform barrl;
+    public float frecency = .9f;
+    private float ctime;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,19 @@ public class sooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       if(ctime<frecency)
+        {
+            ctime += Time.deltaTime;
+
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                GameObject clone = Instantiate(projectil,barrl.position,barrl.rotation);
+                Destroy(clone, 3);
+                ctime = 0;
+            }
+        }
     }
 }
